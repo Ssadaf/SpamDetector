6327a07f662d92b802a8c9a5187c7b7d012cb8c6
Ssadaf <s.sadaf2277@gmail.com>
2019-04-25 19:44:06 +0430

    This commit will implement the prediction and also testing part of spam detector

--- spamDetector.py
+++ spamDetector.py
@@ -1,9 +1,9 @@
 import csv
-# import pandas as pd
 import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+from math import log
 
 def readTrainTestData():
     with open('data/train_test.csv', 'r') as f:
@@ -11,34 +11,24 @@ def readTrainTestData():
         trainTestData = list(reader)
     return trainTestData[1:]
 
-def normalizeText(text):
-    stopWords = set(stopwords.words('english'))
-    punctuation = string.punctuation
-    stemmer = PorterStemmer()
-    
-    text = text.translate(str.maketrans('', '', punctuation))
-    wordTokens = word_tokenize(text)
-    filteredWords = [w.lower() for w in wordTokens if not w in stopWords]
-    stemmedWords = [stemmer.stem(w) for w in filteredWords]
-    return stemmedWords
-
-def refineTextInTrainingData(data):
-    refined = []
-    for item in data:
-        text = normalizeText(item[1])
-        refined.append([item[0], text])    
-    return refined
-
 class SpamDetector:
     def __init__(self, trainData, testData):
         self.trainData = trainData
         self.testData = testData 
 
+    def normalizeText(self, text):
+        stopWords = set(stopwords.words('english'))
+        punctuation = string.punctuation
+        stemmer = PorterStemmer()
+        
+        text = text.translate(str.maketrans('', '', punctuation))
+        wordTokens = word_tokenize(text)
+        filteredWords = [w.lower() for w in wordTokens if not w in stopWords]
+        stemmedWords = [stemmer.stem(w) for w in filteredWords]
+        return stemmedWords
+
     def train(self):
-        self.numSpamWords = 0
-        self.numSpamEmails = 0
-        self.numHamWords = 0
-        self.numHamEmails = 0
+        self.numSpamWords, self.numSpamEmails, self.numHamWords, self.numHamEmails = 0, 0, 0, 0
         self.spamWords = dict()
         self.hamWords = dict()
         
@@ -51,7 +41,7 @@ class SpamDetector:
             else:
                 self.numSpamEmails += 1
             
-            words = normalizeText(text)
+            words = self.normalizeText(text)
             for word in words:
                 if label == 'ham':
                     self.numHamWords += 1
@@ -66,8 +56,8 @@ class SpamDetector:
         self.hamWordsProb = dict()
         self.spamWordsProb = dict()
         numEmails = self.numHamEmails + self.numSpamEmails
-        self.probHam = self.numHamEmails / numEmails
-        self.probSpam = self.numSpamEmails / numEmails 
+        self.hamEmailProb = self.numHamEmails / numEmails
+        self.spamEmailProb = self.numSpamEmails / numEmails 
         self.numDistinctHamWords = len(list(self.hamWords.keys()))
         self.numDistinctSpamWords = len(list(self.spamWords.keys()))
 
@@ -76,6 +66,59 @@ class SpamDetector:
         for word in self.hamWords.keys():
             self.hamWordsProb[word] = ((self.hamWords[word] + 1) / (self.numHamWords + self.numDistinctHamWords))
 
+    def isSpam(self, text):
+        spamProb = log(self.spamEmailProb)
+        hamProb = log(self.hamEmailProb)
+        words = self.normalizeText(text)
+       
+        for word in words:
+            if word in self.spamWordsProb:
+                spamProb += log(self.spamWordsProb[word])
+            else:
+                spamProb += log( 1 / (self.numSpamWords + self.numDistinctSpamWords) )
+           
+            if word in self.hamWordsProb:
+                hamProb += log(self.hamWordsProb[word])
+            else:
+                hamProb += log( 1 / (self.numHamWords + self.numDistinctHamWords) )
+        return spamProb >= hamProb
+    
+    def predict(self, testTexts):
+        result = dict()
+        for i, text in enumerate(testTexts):
+            if self.isSpam(text):
+                result[i] = 'spam'
+            else:
+                result[i] = 'ham'
+        return result
+
+    def test(self):
+        testTexts = [item[1] for item in self.testData]
+        testLabels = [item[0] for item in self.testData]
+        truePositive, trueNegative, falsePositive, falseNegative = 0, 0, 0, 0
+
+        result = self.predict(testTexts)
+        for i, label in enumerate(testLabels):
+            if result[i] == 'spam' and label == 'spam':
+                truePositive += 1
+            elif result[i] == 'ham' and label == 'ham':
+                trueNegative += 1
+            elif result[i] == 'spam' and label == 'ham':
+                falsePositive += 1
+            elif result[i] == 'ham' and label == 'spam':
+                falseNegative += 1
+                
+        self.estimateTrainResult(truePositive, trueNegative, falsePositive, falseNegative)
+
+    def estimateTrainResult(self, truePositive, trueNegative, falsePositive, falseNegative):
+        recall = (truePositive / (truePositive + falseNegative)) * 100
+        precision = (truePositive / (truePositive + falsePositive)) * 100
+        accuracy = ((trueNegative + truePositive) / (trueNegative + truePositive + falseNegative + falsePositive)) * 100
+        
+        print('Recall', recall, '%')
+        print('Precision', precision, '%')
+        print('Accuracy', accuracy, '%')
+
 data = readTrainTestData()
 trainPortion = int(0.75 * len(data))
 trainData = data[0:trainPortion]
@@ -83,3 +126,4 @@ testData = data[trainPortion+1 :]
 
 detector = SpamDetector(trainData, testData)
 detector.train()
+detector.test()
